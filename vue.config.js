6e22141d770c7da9b0f9b45db9197e67b81bed6c
Marlon <717900268@qq.com>
2019-04-17 14:53:16 +0800

    发布到github pages

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  outputDir: 'docs',
+  publicPath: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
